67ed595c929b2d7d2206a33672a86c2fa3b3876b
Michal Charemza <michal@charemza.name>
2017-05-27 11:20:43 +0100

    Bit of point-free style

--- sudoku.hs
+++ sudoku.hs
@@ -30,7 +30,7 @@ niceString matrix = intercalate "\n" $ chunksOf 18 asStrings
     asStrings = intercalate " " $ map (show . head) matrix
 
 isNotSolved :: State GridState Bool
-isNotSolved = get >>= \s -> return $ any (\xs -> length xs > 1) s
+isNotSolved = get >>= \s -> return $ any (((<) 1) . length) s
 
 iteration :: State GridState [()]
 iteration = whileM isNotSolved $ do
