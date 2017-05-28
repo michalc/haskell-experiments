3055134bfb19411d734dea2504b44f90c8d46fc1
Michal Charemza <michal@charemza.name>
2017-05-28 08:11:51 +0100

    Using different types for different uses

--- sudoku.hs
+++ sudoku.hs
@@ -4,6 +4,9 @@ import Control.Monad.State.Strict
 import Data.List
 import Data.List.Split
 
+type SudokuValue = Int
+type MatrixIndex = Int
+
 initial = [
     Nothing, Nothing,  Just 3,   Nothing, Nothing,  Just 7,    Just 1, Nothing, Nothing,
     Nothing,  Just 4,  Just 1,   Nothing,  Just 2, Nothing,   Nothing, Nothing,  Just 5,
@@ -24,25 +27,25 @@ main = putStrLn $ niceString $ snd $ runState iteration $ map toPotential initia
     toPotential Nothing  = [1..9]
     toPotential (Just x) = [x]
 
-niceString :: [[Int]] -> String
+niceString :: [[SudokuValue]] -> String
 niceString matrix = intercalate "\n" $ chunksOf 18 asStrings
   where
     asStrings = intercalate " " $ map (show . head) matrix
 
-iteration :: State [[Int]] ()
+iteration :: State [[SudokuValue]] ()
 iteration = flip untilM_ isSolved $ mapM_ iterationGroup groups
   where
-    iterationGroup matrix = partsOf (traversed . indices (`elem` is)) %= reducePotentials
+    iterationGroup matrix = partsOf (traversed . indices (`elem` matrix)) %= reducePotentials
     isSolved = get >>= return . all (((==) 1) . length)
 
-groups :: [[Int]]
+groups :: [[MatrixIndex]]
 groups = rows ++ columns ++ cells
   where
     rows = chunksOf 9 [0..80]
     columns = transpose rows
     cells = concatMap (map concat . chunksOf 3) $ transpose $ map (chunksOf 3) columns
 
-reducePotentials :: (Eq a) => [[a]] -> [[a]]
+reducePotentials :: [[SudokuValue]] -> [[SudokuValue]]
 reducePotentials subMatrix = map (withoutPotential) subMatrix 
   where
     withoutPotential [x] = [x]
