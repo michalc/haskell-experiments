3305a05a20a7c6798480e1ef0ade6c9f2cc689e0
Michal Charemza <michal@charemza.name>
2017-12-31 13:38:09 +0000

    Revert "Use Bit rather than a list to store the potentials"
    This reverts commit 9847875172ae9d9eb57712a856cdab828a6115be.

--- sudoku.hs
+++ sudoku.hs
@@ -1,7 +1,6 @@
 import Control.Lens
 import Control.Monad.Loops
 import Control.Monad.State.Strict
-import Data.Bits
 import Data.List
 import Data.List.Split
 import Data.Maybe
@@ -10,11 +9,7 @@ data SudokuValue = S1 | S2 | S3 | S4 | S5 | S6 | S7 | S8 | S9 deriving (Eq, Enum
 instance Show SudokuValue where
   show s = show $ fromJust (s `elemIndex` [S1 ..]) + 1
 
-type Potentials = Int
 type MatrixIndex = Int
-
-allPotentials :: Potentials
-allPotentials = foldr (.|.) zeroBits $ map (bit.fromEnum) [S1 ..]
 
 initial = [
     Nothing, Nothing, Just S3,   Nothing, Nothing, Just S7,   Just S1, Nothing, Nothing,
@@ -34,11 +29,11 @@ main :: IO ()
 main = putStrLn $ niceString $ untilStable (execState groupTransforms) $ map toPotential initial
   where
     niceString s
-      | isSolved s = intercalate "\n" $ (chunksOf 18) $ unwords $ map (show.toSudokuValue) s
+      | isSolved s = intercalate "\n" $ (chunksOf 18) $ unwords $ map (show . head) s
       | otherwise  = "Not solveable"
-    isSolved = all ((1 ==) . popCount)
-    toPotential Nothing  = allPotentials
-    toPotential (Just x) = toPotentials x
+    isSolved = all ((1 ==) . length)
+    toPotential Nothing  = [S1 ..]
+    toPotential (Just x) = [x]
 
 untilStable :: Eq a => (a -> a) -> a -> a
 untilStable f a
@@ -53,21 +48,14 @@ groups = rows ++ columns ++ cells
     columns = transpose rows
     cells = concatMap (map concat . chunksOf 3) $ transpose $ map (chunksOf 3) columns
 
-groupTransforms :: State [Potentials] ()
+groupTransforms :: State [[SudokuValue]] ()
 groupTransforms = mapM_ groupTransform groups
   where
     groupTransform group = partsOf (traversed . indices (`elem` group)) %= reducePotentials
 
-reducePotentials :: [Potentials] -> [Potentials]
+reducePotentials :: [[SudokuValue]] -> [[SudokuValue]]
 reducePotentials subMatrix = map withoutPotential subMatrix 
   where
-    withoutPotential xs
-      | popCount xs == 1 = xs
-      | otherwise        = xs .&. (complement certainties)
-    certainties          = foldr (.|.) zeroBits $ filter ((1 ==) . popCount) subMatrix
-
-toPotentials :: SudokuValue -> Potentials
-toPotentials = bit . fromEnum
-
-toSudokuValue :: Potentials -> SudokuValue
-toSudokuValue x = toEnum $ popCount $ x-1
+    withoutPotential [x] = [x]
+    withoutPotential  xs = xs \\ certainties
+    certainties          = subMatrix >>= \x -> case x of [x] -> [x] ; _ -> []
